110173fb72511012e20fc1f99d58163035c690d6
ray <1490493387@qq.com>
2024-09-19 08:22:12 +0800

    添加stylelint规则和测试用例

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,15 @@ onLaunch(() => {
   console.log("App Launch");
 });
 onShow(() => {
-  const number = "123";
-  console.log(
-    "App Show111111111111111111111111111111111111111111111111111111111111123231233333333331111111",
-  );
   console.log("App Hide");
 });
 </script>
 <style lang="scss">
-.search {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+.container {
+  position: relative;
   width: 100%;
   height: 100vh;
+  padding: 1px;
   background-color: #f5f5f5;
-  paddings: 123;
 }
 </style>
